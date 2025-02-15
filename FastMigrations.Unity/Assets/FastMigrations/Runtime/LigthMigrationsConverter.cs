9f59646ab4dd142120bf1c9647e14388103ee201
Ilya Naumov <hairon20152003@mail.ru>
2025-02-15 18:32:26 +0400

    Calculated cognitive complexity for FastMigrationsConverter

--- FastMigrations.Unity/Assets/FastMigrations/Runtime/LigthMigrationsConverter.cs
+++ FastMigrations.Unity/Assets/FastMigrations/Runtime/LigthMigrationsConverter.cs
@@ -23,9 +23,9 @@ namespace FastMigrations.Runtime
     internal delegate JObject MigrateMethod(JObject data);
 
     /*
-     * Operational complexity (Co) = ???
-     * Architectural complexity (Ca) = inputs + outputs + variables = ???
-     * Cognitive complexity = Co * Ca = ???
+     * Operational complexity (Co) = 130 + 28 + 392 + 12 228 + 514 306 + 23 + 106 = 527 213
+     * Architectural complexity (Ca) = inputs + outputs + variables = 2 + 3 + 6 + 9 + 9 + 6 + 10 + (fields)6 = 51
+     * Cognitive complexity = Co * Ca = 527 213 * 51 = 26 887 863
      */
     public class FastMigrationsConverter : JsonConverter
     {
@@ -39,61 +39,64 @@ namespace FastMigrations.Runtime
         private readonly IDictionary<Type, IDictionary<int, MigrateMethod>> _migrateMethodsByType;
         
         /*
-         * Operational complexity (Co) = ???
-         * Architectural complexity (Ca) = inputs + outputs + variables = ???
-         * Cognitive complexity = Co * Ca = ???
+         * Operational complexity (Co) = 113 + 8 + 8 + 1 = 130
+         * Architectural complexity (Ca) = inputs + outputs + variables = 1 + 1 + 0 = 2
+         * Cognitive complexity = Co * Ca = 130 * 2 = 260
          */
         public FastMigrationsConverter(MigratorMissingMethodHandling methodHandling)
         {
-            _migrationInProgress = new ThreadLocal<HashSet<Type>>(() => new HashSet<Type>());
-            _attributeByTypeCache = new ConcurrentDictionary<Type, MigratableAttribute>();
-            _migrateMethodsByType = new ConcurrentDictionary<Type, IDictionary<int, MigrateMethod>>();
+            _migrationInProgress = new ThreadLocal<HashSet<Type>>(() =>
+            {
+                return new HashSet<Type>(); // w = 2, seq = 1, func = 7, W = 2 * (1 + 7) = 16            
+            }); // w = 1, seq = 1, func = 7 * 16 = 112, W = 1 + 112 = 113
+            _attributeByTypeCache = new ConcurrentDictionary<Type, MigratableAttribute>(); // w = 1, seq = 1, func = 7, W = 1 + 7 = 8
+            _migrateMethodsByType = new ConcurrentDictionary<Type, IDictionary<int, MigrateMethod>>(); // w = 1, seq = 1, func = 7, W = 1 + 7 = 8
 
-            _methodHandling = methodHandling;
+            _methodHandling = methodHandling; // w = 1, seq = 1, W = 1
         }
 
         /*
-         * Operational complexity (Co) = ???
-         * Architectural complexity (Ca) = inputs + outputs + variables = ???
-         * Cognitive complexity = Co * Ca = ???
+         * Operational complexity (Co) = 8 + 6 + 6 + 8 = 28
+         * Architectural complexity (Ca) = inputs + outputs + variables = 1 + 1 + 1 = 3
+         * Cognitive complexity = Co * Ca = 28 * 3 = 84
          */
         public override bool CanConvert(Type objectType)
         {
-            MigratableAttribute attribute = GetMigratableAttribute(objectType, _attributeByTypeCache);
+            MigratableAttribute attribute = GetMigratableAttribute(objectType, _attributeByTypeCache); // w = 1, seq = 1, func = 7, W = 1 + 7 = 8
 
-            if (attribute == null)
-                return false;
+            if (attribute == null) // w = 1, branch = 3 * 2 = 6, W = 6 
+                return false;      // w = 2, seq = 1, W = 2 * 1 = 2 
 
-            if (attribute.Version == MigratorConstants.DefaultVersion)
-                return false;
+            if (attribute.Version == MigratorConstants.DefaultVersion) // w = 1, branch = 3 * 2 = 6, W = 6
+                return false;                                          // w = 2, seq = 1, W = 2 * 1 = 2
 
-            return !_migrationInProgress.Value.Contains(objectType);
+            return !_migrationInProgress.Value.Contains(objectType);   // w = 1, seq = 1, func = 7, W = 1 + 7 = 8
         }
 
         /*
-         * Operational complexity (Co) = ???
-         * Architectural complexity (Ca) = inputs + outputs + variables = ???
-         * Cognitive complexity = Co * Ca = ???
+         * Operational complexity (Co) = 8 + 384 = 392
+         * Architectural complexity (Ca) = inputs + outputs + variables = 3 + 0 + 3 = 6
+         * Cognitive complexity = Co * Ca = 392 * 6 = 2352
          */
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            Type valueType = value.GetType();
+            Type valueType = value.GetType(); // w = 1, seq = 1, func = 7, W = 1 + 7 = 8
 
-            try
+            try // w = 1, branch = 3 * (112 + 16) = 384, W = 384
             {
-                if (_migrationInProgress.Value.Contains(valueType))
-                    return;
+                if (_migrationInProgress.Value.Contains(valueType)) // w = 2, branch = 3 * 3 = 9, func = 7, W = 2 * (9 + 7) = 32
+                    return; // w = 3, seq = 1, W = 3 * 1 = 3
 
-                _migrationInProgress.Value.Add(valueType);
+                _migrationInProgress.Value.Add(valueType); // w = 2, seq = 1, func = 7, W = 2 * (1 + 7) = 16
 
-                var jObject = JObject.FromObject(value, serializer);
-                var migratableAttribute = GetMigratableAttribute(valueType, _attributeByTypeCache);
-                jObject.Add(MigratorConstants.VersionJsonFieldName, migratableAttribute.Version);
-                jObject.WriteTo(writer);
+                var jObject = JObject.FromObject(value, serializer); // w = 2, seq = 1, func = 7, W = 2 * (1 + 7) = 16
+                var migratableAttribute = GetMigratableAttribute(valueType, _attributeByTypeCache); // w = 2, seq = 1, func = 7, W = 2 * (1 + 7) = 16
+                jObject.Add(MigratorConstants.VersionJsonFieldName, migratableAttribute.Version); // w = 2, seq = 1, func = 7, W = 2 * (1 + 7) = 16
+                jObject.WriteTo(writer); // w = 2, seq = 1, func = 7, W = 2 * (1 + 7) = 16
             }
             finally
             {
-                _migrationInProgress.Value.Remove(valueType);
+                _migrationInProgress.Value.Remove(valueType); // w = 2, seq = 1, func = 7, W = 2 * (1 + 7) = 16
             }
         }
 
@@ -105,7 +108,7 @@ namespace FastMigrations.Runtime
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
             JsonSerializer serializer)
         {
-            try // w = 2, if = 3 * (4074+2), W = 12228
+            try // w = 1, if = 3 * (4074+2), W = 12228
             {
                 if (_migrationInProgress.Value.Contains(objectType)) // w = 2, if = 3 * 3 = 9, func = 7, W = 2*(9+7) = 32
                     return existingValue; // w = 3 * seq = 3
@@ -150,80 +153,80 @@ namespace FastMigrations.Runtime
         }
 
         /*
-         * Operational complexity (Co) = ???
-         * Architectural complexity (Ca) = inputs + outputs + variables = ???
-         * Cognitive complexity = Co * Ca = ???
+         * Operational complexity (Co) = 1 + 514 304 + 1 = 514 306
+         * Architectural complexity (Ca) = inputs + outputs + variables = 5 + 1 + 3 = 9
+         * Cognitive complexity = Co * Ca = 514 306 * 9 = 4 628 754
          */
         private JObject RunMigrations(JObject jObject, Type objectType, int fromVersion,
             uint toVersion, MigratorMissingMethodHandling methodHandling)
         {
-            fromVersion += MigratorConstants.MinVersionToStartMigration;
+            fromVersion += MigratorConstants.MinVersionToStartMigration; // w = 1, seq = 1, W = 1
 
-            for (int currVersion = fromVersion; currVersion <= toVersion; ++currVersion)
+            for (int currVersion = fromVersion; currVersion <= toVersion; ++currVersion) // w = 1, for-loop = 7 * (16 + 73 440 + 16), W = 514 304
             {
-                var migrationMethod = GetMigrateMethod(objectType, currVersion, _migrateMethodsByType);
+                var migrationMethod = GetMigrateMethod(objectType, currVersion, _migrateMethodsByType); // w = 2, seq = 1, func = 7, W = 2 * (1 + 7) = 16
 
-                if (migrationMethod == null)
+                if (migrationMethod == null) // w = 2, branch = 3 * 12 240 = 36 720, W = 2 * 2448 = 73 440
                 {
-                    switch (methodHandling)
+                    switch (methodHandling) // w = 3, switch = 4 * (960 + 60) = 4080, W = 3 * 4080 = 12 240
                     {
-                        case MigratorMissingMethodHandling.ThrowException:
+                        case MigratorMissingMethodHandling.ThrowException: // w = 4, branch = 3 * 80 = 240, W = 4 * 240 = 960
                         {
-                            var methodName = string.Format(MigratorConstants.MigrateMethodFormat, currVersion);
-                            throw new MigrationException($"Migration method {methodName} not found in {objectType.Name}");
+                            var methodName = string.Format(MigratorConstants.MigrateMethodFormat, currVersion); // w = 5, seq = 1, func = 7, W = 5 * (1 + 7) = 40
+                            throw new MigrationException($"Migration method {methodName} not found in {objectType.Name}"); // w = 5, seq = 1, func = 7, W = 5 * (1 + 7) = 40
                         }
-                        case MigratorMissingMethodHandling.Ignore:
+                        case MigratorMissingMethodHandling.Ignore: // w = 4, branch = 3 * 5 = 15, W = 4 * 15 = 60
                         {
-                            continue;
+                            continue; //w = 5, seq = 1, W = 5
                         }
                     }
                 }
 
-                jObject = migrationMethod(jObject);
+                jObject = migrationMethod(jObject); // w = 2, seq = 1, func = 7, W = 2 * (1 + 7) = 16
             }
-            return jObject;
+            return jObject; // w = 1, seq = 1, W = 1
         }
 
         /*
-         * Operational complexity (Co) = ???
-         * Architectural complexity (Ca) = inputs + outputs + variables = ???
-         * Cognitive complexity = Co * Ca = ???
+         * Operational complexity (Co) = 13 + 8 + 1 + 1 = 23
+         * Architectural complexity (Ca) = inputs + outputs + variables = (1 + 3) + 1 + 1 = 6
+         * Cognitive complexity = Co * Ca = 23 * 6 = 138
          */
         private static MigratableAttribute GetMigratableAttribute(Type objectType, IDictionary<Type, MigratableAttribute> cache)
         {
-            if (cache.TryGetValue(objectType, out MigratableAttribute attribute))
-                return attribute;
+            if (cache.TryGetValue(objectType, out MigratableAttribute attribute)) // w = 1, branch = 3 * 2 = 6, func = 7, W = 6 + 7 = 13  
+                return attribute; // w = 2, seq = 1, W = 2
 
-            attribute = (MigratableAttribute)objectType.GetCustomAttribute(typeof(MigratableAttribute), false);
-            cache[objectType] = attribute;
-            return attribute;
+            attribute = (MigratableAttribute)objectType.GetCustomAttribute(typeof(MigratableAttribute), false); // w = 1, seq = 1, func = 7, W = 1 + 7 = 8
+            cache[objectType] = attribute; // w = 1, seq = 1, W = 1
+            return attribute; // w = 1, seq = 1, W = 1
         }
 
         /*
-         * Operational complexity (Co) = ???
-         * Architectural complexity (Ca) = inputs + outputs + variables = ???
-         * Cognitive complexity = Co * Ca = ???
+         * Operational complexity (Co) = 61 + 13 + 8 + 8 + 6 + 8 + 1 + 1 = 106
+         * Architectural complexity (Ca) = inputs + outputs + variables = (1 + 1 + 3) + 1 + 4 = 10
+         * Cognitive complexity = Co * Ca = 106 * 10 = 1060
          */
         private static MigrateMethod GetMigrateMethod(Type objectType, int version, IDictionary<Type, IDictionary<int, MigrateMethod>> cache)
         {
-            if (!cache.TryGetValue(objectType, out IDictionary<int, MigrateMethod> methodsByVersion))
+            if (!cache.TryGetValue(objectType, out IDictionary<int, MigrateMethod> methodsByVersion)) // w = 1, branch = 3 * (16 + 2) = 54, func = 7, W = 54 + 7 = 61 
             {
-                methodsByVersion = new ConcurrentDictionary<int, MigrateMethod>();
-                cache[objectType] = methodsByVersion;
+                methodsByVersion = new ConcurrentDictionary<int, MigrateMethod>(); // w = 2, seq = 1, func = 7, W = 2 * (1 + 7) = 16
+                cache[objectType] = methodsByVersion; // w = 2, seq = 1, W = 2
             }
 
-            if (methodsByVersion.TryGetValue(version, out MigrateMethod method))
-                return method;
+            if (methodsByVersion.TryGetValue(version, out MigrateMethod method)) // w = 1, branch = 3 * 2 = 6, func = 7, W = 6 + 7 = 13
+                return method; // w = 2, seq = 1, W = 2
 
-            var methodName = string.Format(MigratorConstants.MigrateMethodFormat, version);
-            var methodInfo = objectType.GetMethod(methodName, BindingFlags.Static | BindingFlags.NonPublic);
+            var methodName = string.Format(MigratorConstants.MigrateMethodFormat, version); // w = 1, seq = 1, func = 7, W = 1 + 7 = 8
+            var methodInfo = objectType.GetMethod(methodName, BindingFlags.Static | BindingFlags.NonPublic); // w = 1, seq = 1, func = 7, W = 1 + 7 = 8
 
-            if (methodInfo == null)
-                return null;
+            if (methodInfo == null) // w = 1, branch = 3 * 2 = 6, W = 6
+                return null; // w = 2, seq = 1, W = 2
 
-            MigrateMethod newMethodDelegate = (MigrateMethod)methodInfo.CreateDelegate(typeof(MigrateMethod));
-            methodsByVersion[version] = newMethodDelegate;
-            return newMethodDelegate;
+            MigrateMethod newMethodDelegate = (MigrateMethod)methodInfo.CreateDelegate(typeof(MigrateMethod)); // w = 1, seq = 1, func = 7, W = 1 + 7 = 8
+            methodsByVersion[version] = newMethodDelegate; // w = 1, seq = 1, W = 1
+            return newMethodDelegate; // w = 1, seq = 1, W = 1
         }
     }
 }
